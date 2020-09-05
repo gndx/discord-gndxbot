9a8e3edc2d14a3ded6b002cfee91849becdf055a
Andres Fernando <andres.fernando08@gmail.com>
2020-09-04 23:37:40 -0500

    [Add] get started a template in order to show messages in the channel from a json file

--- src/index.js
+++ src/index.js
@@ -4,20 +4,33 @@ const messageEmbed = require('./utils/messageEmbed');
 
 require('dotenv').config();
 
+const rules = require('./messages/rules.json');
+
+
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 
 client.on('message', msg => {
   console.log(msg.content)
 
-  if (msg.channel.type === 'dm') {
-    console.log('Enviaste un DM.')
-    msg.reply(':thinking:');
-    msg.react('👍');
-  }
+  // if (msg.channel.type === 'dm') {
+  //   console.log('Enviaste un DM.')
+  //   msg.reply(':thinking:');
+  //   msg.react('👍');
+  // }
 
-  if (msg.content.toLocaleLowerCase() === 'hola') {
-    msg.channel.send(messageEmbed());
-    msg.react('👍');
+  // if (msg.content.toLocaleLowerCase() === 'hola') {
+  //   msg.channel.send(messageEmbed());
+  //   msg.react('👍');
+  // }
+
+  if (msg.content.toLocaleLowerCase() === '!reglas') {
+    const author = msg.author.username
+    // msg.channel.send(`Estas son las reglas, ${author}`)
+    msg.channel.send(messageEmbed({
+      authorName: author,
+      title: rules.customMessage,
+      content: rules.rules
+    }))
   }
 
 });
--- src/utils/messageEmbed.js
+++ src/utils/messageEmbed.js
@@ -1,10 +1,12 @@
 const Discord = require('discord.js');
 
-const messageEmbed = () => {
+const messageEmbed = ({ authorName, title, content }) => {
+  const parseContent = content.map(i => `${i}\n`)
   const msgEmbed = new Discord.MessageEmbed()
     .setColor('#0099ff')
-    .setTitle('Hola soy GndxBot')
-    .setDescription('Hola Mundo')
+    .setTitle(`Hola ${authorName}`)
+    .setDescription(`${title} ${parseContent}`)
+    .setFooter('Comunidad de GNDX')
 
   return msgEmbed;
 };

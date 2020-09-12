4940cab42eed2c999bd0961249fde292deaa6840
Oscar Barajas <oscar@arepa.dev>
2020-09-11 22:25:31 -0500

    [add] procfile

--- src/index.js
+++ src/index.js
@@ -6,33 +6,18 @@ require('dotenv').config();
 
 const rules = require('./messages/rules.json');
 
-
 const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 
 client.on('message', msg => {
   console.log(msg.content)
-
-  // if (msg.channel.type === 'dm') {
-  //   console.log('Enviaste un DM.')
-  //   msg.reply(':thinking:');
-  //   msg.react('👍');
-  // }
-
-  // if (msg.content.toLocaleLowerCase() === 'hola') {
-  //   msg.channel.send(messageEmbed());
-  //   msg.react('👍');
-  // }
-
   if (msg.content.toLocaleLowerCase() === '!reglas') {
     const author = msg.author.username
-    // msg.channel.send(`Estas son las reglas, ${author}`)
     msg.channel.send(messageEmbed({
       authorName: author,
       title: rules.customMessage,
       content: rules.rules
     }))
   }
-
 });
 
 client.on('ready', () => {
